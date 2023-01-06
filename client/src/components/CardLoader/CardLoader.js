470f865171fa6c2d39c143628f264e72fc610135
solch4 <sol.maldonado.0209@gmail.com>
2023-01-06 01:05:03 -0300

    card loader updated

--- client/src/components/CardLoader/CardLoader.js
+++ client/src/components/CardLoader/CardLoader.js
@@ -2,80 +2,22 @@ import React from 'react';
 import { container, card, img, body, title, diet } from './CardLoader.module.css'
 
 function CardLoader() {
+  const nOfCards = 9;
+  const arrWithAmountOfCards = [];
+  for (let i = 1; i <= nOfCards; i++) arrWithAmountOfCards.push(i);
+
   return (
     <div className={container}>
-      <div className={card}>
-        <div className={img}></div>
-        <div className={body}>
-          <div className={title}></div>
-          <div className={diet}></div>
-          <div className={diet}></div>
+      {arrWithAmountOfCards.map((e) => (
+        <div className={card} key={e}>
+          <div className={img}></div>
+          <div className={body}>
+            <div className={title}></div>
+            <div className={diet}></div>
+            <div className={diet}></div>
+          </div>
         </div>
-      </div>
-      <div className={card}>
-        <div className={img}></div>
-        <div className={body}>
-          <div className={title}></div>
-          <div className={diet}></div>
-          <div className={diet}></div>
-        </div>
-      </div>
-      <div className={card}>
-        <div className={img}></div>
-        <div className={body}>
-          <div className={title}></div>
-          <div className={diet}></div>
-          <div className={diet}></div>
-        </div>
-      </div>
-      <div className={card}>
-        <div className={img}></div>
-        <div className={body}>
-          <div className={title}></div>
-          <div className={diet}></div>
-          <div className={diet}></div>
-        </div>
-      </div>
-      <div className={card}>
-        <div className={img}></div>
-        <div className={body}>
-          <div className={title}></div>
-          <div className={diet}></div>
-          <div className={diet}></div>
-        </div>
-      </div>
-      <div className={card}>
-        <div className={img}></div>
-        <div className={body}>
-          <div className={title}></div>
-          <div className={diet}></div>
-          <div className={diet}></div>
-        </div>
-      </div>
-      <div className={card}>
-        <div className={img}></div>
-        <div className={body}>
-          <div className={title}></div>
-          <div className={diet}></div>
-          <div className={diet}></div>
-        </div>
-      </div>
-      <div className={card}>
-        <div className={img}></div>
-        <div className={body}>
-          <div className={title}></div>
-          <div className={diet}></div>
-          <div className={diet}></div>
-        </div>
-      </div>
-      <div className={card}>
-        <div className={img}></div>
-        <div className={body}>
-          <div className={title}></div>
-          <div className={diet}></div>
-          <div className={diet}></div>
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
